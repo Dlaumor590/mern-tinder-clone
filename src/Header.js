b6ca39e6dfd1c67691d99a999b2298b601408de1
romuald <rgnossa729@gmail.com>
2021-05-18 20:44:11 +0000

    finished the header component

--- src/Header.js
+++ src/Header.js
@@ -2,6 +2,7 @@ import React from 'react';
 import './Header.css';
 import PersonPinIcon from '@material-ui/icons/PersonPin';
 import IconButton from '@material-ui/core/IconButton';
+import ForumRoundedIcon from '@material-ui/icons/ForumRounded';
 
 function Header() {
     return (
@@ -9,7 +10,10 @@ function Header() {
             <IconButton>
                 <PersonPinIcon fontSize='large' className='header__icon' />
             </IconButton>
-            <img className='header__logo' src='https://cdn.designrush.com/uploads/inspirations/2354/crop_683_410__1513706350_604_tin.png' />
+            <img className='header__logo' src='https://cdn.designrush.com/uploads/inspirations/2354/crop_683_410__1513706350_604_tin.png' alt='' />
+            <IconButton>
+                <ForumRoundedIcon fontSize='large' className='header__icon'/>
+            </IconButton>
         </div>
     )
 }
