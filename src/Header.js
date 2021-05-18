3ed7fe368af01b5c3c1bf362ba4225f460ea2b2a
romuald <rgnossa729@gmail.com>
2021-05-18 20:26:52 +0000

    styling the header

--- src/Header.js
+++ src/Header.js
@@ -1,10 +1,15 @@
-import React from 'react'
-import './Header.css'
+import React from 'react';
+import './Header.css';
+import PersonPinIcon from '@material-ui/icons/PersonPin';
+import IconButton from '@material-ui/core/IconButton';
 
 function Header() {
     return (
         <div className='header'>
-            <h1>I am the header</h1>
+            <IconButton>
+                <PersonPinIcon fontSize='large' className='header__icon' />
+            </IconButton>
+            <img className='header__logo' src='https://cdn.designrush.com/uploads/inspirations/2354/crop_683_410__1513706350_604_tin.png' />
         </div>
     )
 }
