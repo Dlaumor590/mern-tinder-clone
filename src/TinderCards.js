520b3db23a7e55e4950e1a7cf1539b8f04317829
romuald <rgnossa729@gmail.com>
2021-05-19 14:56:13 +0000

    add swipe buttons

--- src/TinderCards.js
+++ src/TinderCards.js
@@ -7,23 +7,24 @@ function TinderCards() {
     const [people, setPeople] = useState([
         {
             name: 'Elon Musk',
-            url:'https://upload.wikimedia.org/wikipedia/commons/e/ed/Elon_Musk_Royal_Society.jpg',
+            url: 'https://upload.wikimedia.org/wikipedia/commons/e/ed/Elon_Musk_Royal_Society.jpg',
         },
         {
             name: 'Jeff Bezos',
-            url:'https://www.forbes.fr/wp-content/uploads/2021/04/gettyimages-950750900-1.jpg',
+            url: 'https://www.forbes.fr/wp-content/uploads/2021/04/gettyimages-950750900-1.jpg',
         }
-    ])
+    ]);
+    
     const swiped = (direction, nameToDelete) => {
         console.log('removing :' + nameToDelete);
-        //setLastDirection(direction);
     };
+    
     const outOfFrame = (name) => {
         console.log(name + ' left the screen!!');
     };
     return (
-        <div className='tinderCards' >
-            <div className='tinderCards__cardContainer' >
+        <div className='tinderCards'>
+            <div className='tinderCards__cardContainer'>
             {people.map(person => (
                 <TinderCard
                     className='swipe'
