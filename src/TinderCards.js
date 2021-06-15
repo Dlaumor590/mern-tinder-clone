d1f24d8e9ba67fe59645d41a629b6fa374fd73b2
romuald <rgnossa729@gmail.com>
2021-06-15 12:05:31 +0000

    add doubles quotes to cards links

--- src/TinderCards.js
+++ src/TinderCards.js
@@ -6,20 +6,20 @@ import './TinderCards.css';
 function TinderCards() {
     const [people] = useState([
         {
-            name: 'Elon Musk',
-            url: 'https://cdn.pixabay.com/photo/2021/05/02/02/05/elon-musk-6222396_960_720.jpg',
+            name: "Elon Musk",
+            url: "https://cdn.pixabay.com/photo/2021/05/02/02/05/elon-musk-6222396_960_720.jpg",
         },
         {
-            name: 'Jeff Bezos',
-            url: 'https://www.forbes.fr/wp-content/uploads/2021/04/gettyimages-950750900-1.jpg',
+            name: "Jeff Bezos",
+            url: "https://www.forbes.fr/wp-content/uploads/2021/04/gettyimages-950750900-1.jpg",
         },
         {
-            name: 'Mia Khalifa',
-            url:'https://cdn.pixabay.com/photo/2021/04/08/09/14/actress-6161118_960_720.png',
+            name: "Mia Khalifa",
+            url:"https://cdn.pixabay.com/photo/2021/04/08/09/14/actress-6161118_960_720.png",
         },
         {
-            name: 'Diane Doni',
-            url:'https://scontent.flfw1-1.fna.fbcdn.net/v/t1.6435-9/52666800_847358125602191_8494094316640141312_n.jpg?_nc_cat=104&ccb=1-3&_nc_sid=09cbfe&_nc_ohc=J6I1bLUjiaUAX9sQTuG&_nc_ht=scontent.flfw1-1.fna&oh=955f083c2e7188654740a04db87d6220&oe=60E36EC3',
+            name: "Diane Doni",
+            url:"https://scontent.flfw1-1.fna.fbcdn.net/v/t1.6435-9/52666800_847358125602191_8494094316640141312_n.jpg?_nc_cat=104&ccb=1-3&_nc_sid=09cbfe&_nc_ohc=J6I1bLUjiaUAX9sQTuG&_nc_ht=scontent.flfw1-1.fna&oh=955f083c2e7188654740a04db87d6220&oe=60E36EC3",
         }
 
     ]);
